101144c07efd19c04658b93a34ee86c6ae668bd0
Rahmawati <115470125+Rhmawthy@users.noreply.github.com>
2022-12-31 13:20:35 +0800

    Add files via upload

---
+++ menentukan fungsi x.py
@@ -0,0 +1,9 @@
+#menghitung f(x)= 2x² +5x * 3/x. di ketahui x = nilai inputan user
+
+print("menentukan f(x)= 2x² +5x * 3/x. di ketahui x = nilai inputan user")
+
+x = int(input("nilai x :"))
+
+f = 2 * x**2 + 5*x * 3/x
+
+print(f)
